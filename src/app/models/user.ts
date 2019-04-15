30c50f43195aba6b7230639930d5880e12603fe9
Christoffer Lundström <lulleh_@hotmail.com>
2019-04-15 15:38:00 +0200

    added user model and enums for userlevel and sex

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,24 @@
+import {Userlevel} from './userlevel';
+import {Sex} from "./sex";
+import DateTimeFormat = Intl.DateTimeFormat;
+
+
 export class User {
+
+  private userLevel: Userlevel;
+  private id: number;
+  private firstName: String;
+  private lastName: String;
+  private city: string;
+  private socialSecurityNr: number;
+  private address: String;
+  private zipCode: number;
+  private sex: Sex;
+  private lastLogin: DateTimeFormat;
+  private loggedIn: boolean;
+
+
+
 
 
 }
---
+++ src/app/models/userlevel.ts
@@ -0,0 +1,3 @@
+export enum Userlevel {
+  Admin, User, Guest
+}
