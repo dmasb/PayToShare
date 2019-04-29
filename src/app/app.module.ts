c9f64eb4945bfad41f905a105a82963f52e11c42
Christoffer Lundström <lulleh_@hotmail.com>
2019-04-29 09:36:59 +0200

    tiny fixe to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,6 @@ import {UpdateProductComponent} from './components/body/admin/productmanagement/
 import {ProductOverviewComponent} from './components/body/admin/productmanagement/overview/product-overview.component';
 import { PanelComponent } from './components/body/admin/panel/panel.component';
 import { ProductsComponent } from './components/body/products/products.component';
-import { SidepanelComponent } from './components/navbar/sidepanel/sidepanel.component';
 
 @NgModule({
   declarations: [
@@ -60,8 +59,7 @@ import { SidepanelComponent } from './components/navbar/sidepanel/sidepanel.comp
     DeleteProductComponent,
     AddProductComponent,
     PanelComponent,
-    ProductsComponent,
-    SidepanelComponent
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
