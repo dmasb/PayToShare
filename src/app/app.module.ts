c26c5b060ca942ff349a16cb1d227de0b085c0cc
Christoffer Lundström <lulleh_@hotmail.com>
2019-05-10 10:27:51 +0200

    merged cartdropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,7 @@ import {AddTagSaleComponent} from './components/body/admin/salemanegement/add-ta
 import {AddPlanSaleComponent} from './components/body/admin/salemanegement/add-plan-sale/add-plan-sale.component';
 import {SaleOverviewComponent} from './components/body/admin/salemanegement/sale-overview/sale-overview.component';
 import { DeleteSaleComponent } from './components/body/admin/salemanegement/delete-sale/delete-sale.component';
+import { CartdropdownComponent } from './components/navbar/cartdropdown/cartdropdown.component';
 
 
 @NgModule({
@@ -96,6 +97,7 @@ import { DeleteSaleComponent } from './components/body/admin/salemanegement/dele
     AddPlanSaleComponent,
     SaleOverviewComponent,
     DeleteSaleComponent,
+    CartdropdownComponent,
   ],
   imports: [
     BrowserModule,
