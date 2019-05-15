ed2b64a600023ad1b890a0346bfbe89848b353bd
David Mozart Andraws <dm222is@student.lnu.se>
2019-05-15 13:51:21 +0200

    Remodelled plans. They now include default licenses

--- src/app/models/products/plan.ts
+++ src/app/models/products/plan.ts
@@ -1,13 +1,13 @@
-import {License} from './license';
 import {Item} from './item';
+import {Level} from './planLevels';
 
 export class Plan implements Item {
   id?: string;
   title: string;
   speed: number;
+  level: Level;
   price: number;
-  licenses: License[];
-  quantity: number;
+  licenses: string[];
   description: string;
   imageUrl: string;
   created: firebase.firestore.Timestamp;
@@ -15,8 +15,8 @@ export class Plan implements Item {
   constructor() {
     this.title = 'Plan name';
     this.speed = 0;
+    this.level = new Level();
     this.price = 0;
-    this.quantity = 0;
     this.description = 'Plan Description';
     this.imageUrl = 'https://firebasestorage.googleapis.com/v0/b/paytoshare-b4cd1.appspot.com/o/' +
       'productImage%2Fitemimg.svg?alt=media&token=130ed9f0-6e1a-4d93-abf3-62d77de18599';
