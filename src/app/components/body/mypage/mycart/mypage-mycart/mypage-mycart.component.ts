58b08406ea6232114dda2dc424632b3c79bf94db
Marcus Thornemo Larsson <ml224du@student.lnu.se>
2019-05-17 12:11:28 +0200

    small changes in mypage

--- src/app/components/body/mypage/mycart/mypage-mycart/mypage-mycart.component.ts
+++ src/app/components/body/mypage/mycart/mypage-mycart/mypage-mycart.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {UserSessionService} from "../../../../../services/user-session.service";
-import {Cart} from "../../../../../models/products/cart";
+import { Component, OnInit } from '@angular/core';
+import { UserSessionService } from 'src/app/services/user-session.service';
+import { Cart } from 'src/app/models/products/cart';
+import { Product } from 'src/app/models/products/product';
+import {User} from "../../../../../models/user";
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -9,12 +12,40 @@ import {Cart} from "../../../../../models/products/cart";
   styleUrls: ['./mypage-mycart.component.scss']
 })
 export class MypageMycartComponent implements OnInit {
-  private cart: Cart;
+  private cart: Cart = new Cart();
+  
 
-  constructor(private session: UserSessionService) { }
+  constructor(private session: UserSessionService, private router: Router) { }
 
   ngOnInit() {
-   
+    this.session.getUserDoc().subscribe(user => {
+
+      this.cart= Cart.clone(user.cart);
+    });
+
+  }
+
+  // GET CART FROM DB
+
+  // UPDATE LOCALLY
+
+  // UPDATE CART
+
+
+  increaseQuantity(product: Product){
+    this.cart.add(product);
+    this.session.updateCart(this.cart);
+  }
+  decreaseQuantity(product: Product){
+    this.cart.remove(product);
+    this.session.updateCart(this.cart);
+  }
+  removeAll(product: Product){
+    this.cart.removeAllOf(product);
+    this.session.updateCart(this.cart);
+  }
+  goToCart() {
+    this.router.navigate(['/checkout']);
   }
 
 }
