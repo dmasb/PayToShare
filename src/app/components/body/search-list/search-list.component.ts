80fd211e27df53a644e10fd5db337e40a303b209
David Mozart Andraws <dm222is@student.lnu.se>
2019-05-22 19:16:58 +0200

    search functionality

--- src/app/components/body/search-list/search-list.component.ts
+++ src/app/components/body/search-list/search-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { SearchbarComponent } from '../../navbar/searchbar/searchbar.component';
-import { ProductsService } from 'src/app/services/crud/products.service';
-import { Router } from '@angular/router';
-import { Product } from 'src/app/models/products/product';
-import { Tag } from 'src/app/models/products/tag';
+import {Component, OnInit, Input} from '@angular/core';
+import {ProductsService} from 'src/app/services/crud/products.service';
+import {Router} from '@angular/router';
+import {Product} from 'src/app/models/products/product';
+import {ActivatedRoute} from '@angular/router';
+import {activateRoutes} from '@angular/router/src/operators/activate_routes';
 
 @Component({
   selector: 'app-search-list',
@@ -12,37 +12,25 @@ import { Tag } from 'src/app/models/products/tag';
 })
 export class SearchListComponent implements OnInit {
 
-@Input() searchText: string;
+  @Input() searchText: string;
 
-  private products: Product[];   
-  
-  
- /*  filteredProducts: Product[];
-  private _searchTerm: string; */
+  private products: Product[];
 
-/*   get searchTerm(): string {
-    return this._searchTerm;
-  }
-  set searchTerm(value: string){
-    this._searchTerm = value;
-    this.filteredProducts = this.filterProducts(value);    
-  }
-
-  filterProducts(searchString: Tag["name"]){
-    return this.products.filter( product => product.tags.indexOf(searchString.toLowerCase) !== -1 );
-  } */
-
-
-  constructor(private router: Router, private ps: ProductsService) { }
-
-  getProducts(): Product[] {
-
-    this.products.filter(this.searchText);
-
+  constructor(private router: Router,
+              private ps: ProductsService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.ps.getProductsByTag([]).subscribe(products => this.products = products);
   }
 
+
+  getProducts(): Product[] {
+    const keyword = this.route.snapshot.paramMap.get('searchWord');
+    return this.products.filter(product => {
+      return product.title.includes(keyword) ||
+        product.tags.filter(tag => tag.name.includes(keyword)).length > 0;
+    });
+  }
 }
--- src/app/components/navbar/searchbar/searchbar.component.ts
+++ src/app/components/navbar/searchbar/searchbar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Product} from '../../../models/products/product';
 import {Tag} from '../../../models/products/tag';
-import { ProductsService } from 'src/app/services/crud/products.service';
+import {ProductsService} from 'src/app/services/crud/products.service';
 
 
 @Component({
@@ -13,30 +13,30 @@ import { ProductsService } from 'src/app/services/crud/products.service';
 export class SearchbarComponent implements OnInit {
   products: Product[];
   filteredProducts: Product[];
-  private _searchTerm: string;
+  private searchTerm: string;
 
-  get searchTerm(): string {
-    return this._searchTerm;
-  }
-  set searchTerm(value: string){
-    this._searchTerm = value;
-    this.filteredProducts = this.filterProducts(value);    
+  /*get searchTerm(): string {
+    return this.searchTerm;
   }
 
-  filterProducts(searchString: Tag["name"]){
-    return this.products.filter( product => product.tags.indexOf(searchString.toLowerCase) !== -1 );
+  set searchTerm(value: string) {
+    this.searchTerm = value;
+    this.filteredProducts = this.filterProducts(value);
+  }*/
+
+  filterProducts(searchString: Tag['name']) {
+    return this.products.filter(product => product.tags.indexOf(searchString.toLowerCase) !== -1);
   }
 
-  constructor(private router: Router, private ps: ProductsService) { }
+  constructor(private router: Router, private ps: ProductsService) {
+  }
 
   ngOnInit() {
-
- 
 
   }
 
   listSearch() {
-    this.router.navigate(['/search']);
-  }
 
+    this.router.navigate([`/search/${this.searchTerm}`]);
+  }
 }
