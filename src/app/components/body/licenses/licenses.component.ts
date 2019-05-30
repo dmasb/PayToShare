9ffe504b2f68e95e8a0572eef234c2c40f639c55
benqav <aq222ae@student.lnu.se>
2019-05-30 04:26:04 +0200

    Changed Profile background. CSS changes to products/licenses. Some more minor changes. ALSO added sale to navbar.
    To-do: Scaling.......

--- src/app/components/body/licenses/licenses.component.ts
+++ src/app/components/body/licenses/licenses.component.ts
@@ -3,7 +3,8 @@ import {LicenseService} from '../../../services/product/license.service';
 import {License} from '../../../models/products/license';
 import {Cart} from '../../../models/products/cart';
 import {UserSessionService} from '../../../services/user-session.service';
-import {Product} from '../../../models/products/product';
+import {StarService} from '../../../services/product/star.service';
+import {Rating} from '../../../models/rating';
 
 @Component({
   selector: 'app-licenses',
@@ -14,13 +15,21 @@ export class LicensesComponent implements OnInit {
 
   private licenses: License[];
   private cart: Cart;
+  private userId: string;
+  private licId: any;
+  private ratings: Rating[];
+
+
   constructor(private licenseService: LicenseService,
-              private userSessionService: UserSessionService) { }
+              private userSessionService: UserSessionService,
+              private starService: StarService,) { }
 
   ngOnInit() {
     this.cart = new Cart();
+    this.starService.getRatings().subscribe(ratings => this.ratings = ratings);
     this.licenseService.getLicenses().subscribe(licenses => this.licenses = licenses);
     this.userSessionService.getUserDoc().subscribe(user => {
+      this.userId = user.id;
       this.cart = Cart.clone(user.cart);
     });
   }
@@ -28,5 +37,37 @@ export class LicensesComponent implements OnInit {
   add(license: License) {
     this.cart.add(license);
     this.userSessionService.updateCart(this.cart);
+  }
+
+  consLog(licenseId) {
+    this.licId = licenseId;
+  }
+
+  get licenseId() {
+    return this.licId;
+  }
+
+  getObjectRating(objectID: string): number {
+    if (this.ratings) {
+      const objectRatings = this.ratings.filter(res => res.objectID === objectID);
+      const numberArray = objectRatings.map(r => r.value);
+      return numberArray.length ? numberArray.reduce((total, val) => total + val) / numberArray.length : 0;
+    }
+  }
+  sort() {
+    const selectedOption = (document.getElementById('sortSales') as HTMLSelectElement);
+
+    switch (selectedOption.value) {
+      case 'Asc': {
+        console.log('asc');
+        this.licenses.sort((a, b) => (this.getObjectRating(a.id) > this.getObjectRating(b.id) ? -1 : 1));
+        break;
+      }
+      case 'Desc': {
+        console.log('Desc');
+        this.licenses.sort((a, b) => (this.getObjectRating(a.id) < this.getObjectRating(b.id) ? -1 : 1));
+        break;
+      }
+    }
   }
 }
